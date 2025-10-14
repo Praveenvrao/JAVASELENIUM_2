fcd6d869a57b8bf9b6857ece17741958d10913ac
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-14 16:39:13 +0530

    DAY1 Changes

---
+++ src/DAY1/Basic_Program1.java
@@ -0,0 +1,14 @@
+package DAY1;
+
+public class Basic_Program1 {
+	
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 30;
+		int c = a+b;
+		System.out.println("This is just basic program and day1");
+		System.out.println("The sum of 10 and 30 is " + c );
+	}
+	
+
+}
