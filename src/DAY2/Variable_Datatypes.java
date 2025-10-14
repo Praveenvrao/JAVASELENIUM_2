0bd1736143890f0dd41bc14418344bb537a112c2
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-15 02:57:13 +0530

    Variables & Data types practice

---
+++ src/DAY2/Variable_Datatypes.java
@@ -0,0 +1,37 @@
+package DAY2;
+
+public class Variable_Datatypes {
+	
+	public static void main(String a[]) {
+		
+		byte b = 127;
+		short s = 133;
+		int i = 500;
+		long l = 56777;
+		long sum = b+s+i+l;
+		
+		float f = 6.7f;
+		double d = 789.7654;
+		
+		char c = 'P';
+		char c1 = 'J';
+		
+		boolean B = true;
+		
+		System.out.println("the sum of all integers is " + sum);
+		System.out.println("And the above sum is "+ B);
+		
+		main1(a);
+	}
+	
+	public static void main1(String a[] )
+	{
+		int x = 44, y = 55, z = 66;
+		int p,q,r;
+		p=111; q=222; r = 333;
+		
+		System.out.println("Above variables assgined are correct");
+		
+	}
+
+}
