6c1a695902f82d768d503cad0260c5785f7159f4
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-15 13:42:59 +0530

    DAY2_Relational operaters program 5

---
+++ src/DAY2/RELATIONAL_OPERATIONS.java
@@ -0,0 +1,17 @@
+package DAY2;
+
+public class RELATIONAL_OPERATIONS {
+	public static void main(String a[]) {
+		System.out.println("Basic Relational operational program");
+		
+		int A =400, B =200;
+		System.out.println("A is equal to B - " + (A==B));
+		System.out.println("A is not equal to B - " + (A!=B));
+		System.out.println("A is greater than B - " + (A>B));
+		System.out.println("A is less than B - "+ (A<B));
+		System.out.println("A is greater than or equal to B - "+ (A>=B));
+		System.out.println("A is less than or equal to B - "+ (A<=B));
+		
+	}
+
+}
