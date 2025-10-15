4fe0d78897315e5fd4a9835eeafe2c9fb1b2c79f
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-15 13:18:33 +0530

    DAY2 Prorgram 3

---
+++ src/DAY2/Initialisationwithoutvalue.java
@@ -0,0 +1,15 @@
+package DAY2;
+
+public class Initialisationwithoutvalue {
+
+	public static void main(String[] args) {
+		
+		int number; //initialisation without value declaration
+		// System.out.println("The value of number " + number);
+		
+		number = 111; 
+		System.out.println("Value after declaration is "+ number);
+
+	}
+
+}
