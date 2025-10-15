c0f6c3b2c4ace18d83c17d6d670eff5c97ed3a47
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-15 13:08:56 +0530

    DAY2_Swapping numbers

---
+++ src/DAY2/Swappingnumbers.java
@@ -0,0 +1,24 @@
+package DAY2;
+
+public class Swappingnumbers {
+	
+	public static void main(String a[]) {
+		
+		// Swapping 2 numbers using +, - Operators
+		
+		int x = 10, y = 22;
+		System.out.println("x = "+ x);
+		System.out.println("y = "+ y);
+		x = x+y;
+		y = x-y;
+		x = x-y;
+		System.out.println("Values after swaspping are below");
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		
+		
+		
+		
+	}
+
+}
