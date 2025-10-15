cdb8a84e6baba47b49fb9e487c9ce671647febcc
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-15 13:33:58 +0530

    DAY2 Program_4 simple operations

---
+++ src/DAY2/BASICARITHEMATIC.java
@@ -0,0 +1,18 @@
+package DAY2;
+
+public class BASICARITHEMATIC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Basic arithmatic operations with 2 numbers are below");
+		
+		int a = 20, b=50;
+		System.out.println("The Addition of 2 numbers - " + a+b);
+		System.out.println("The Substraction of 2 numbers - " + (a-b));
+		System.out.println("The Multiplication of 2 numbers - " + a*b);
+		System.out.println("The Division of 2 numbers - " + a/b);
+		System.out.println("The Module of 2 numbers - " + b%a);
+
+	}
+
+}
