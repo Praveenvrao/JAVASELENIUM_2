3587165abb2bb091fe6a6aaa4f80cfa57dd7ef3b
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-17 18:46:10 +0530

    DAY3_Loops_Program_5

--- src/DAY3/For_loop_reversetriangle.java
+++ src/DAY3/For_loop_reversetriangle.java
@@ -6,15 +6,25 @@ public class For_loop_reversetriangle {
 		// TODO Auto-generated method stub
 		
 		// Print reverse right angle triangle with stars
-		 int rows = 6;
+		 /*int rows = 6;
 		 
 		 for (int i = rows ; i >=1; i--) {
 			 for (int j = 1; j<=i; j++) {
 				 System.out.print(" * ");
 			 }
 			 System.out.println();
-		 }
+		 }*/
 		
+		/*int rows = 6;
+		
+		for (int i = 1 ; i <=rows; i++) {
+			for(int j = 1; j<=i ; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}*/
+		
+		int rows 
 
 	}
 
