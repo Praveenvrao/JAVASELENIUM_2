ec3185811157d860f60941b048ba57518f20fc1e
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-17 18:08:42 +0530

    Loops_for loops_3

---
+++ src/DAY3/For_Loop.java
@@ -0,0 +1,18 @@
+package DAY3;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Input any integer and print its multiplication table (from 1 to 10) using a for loop.
+		
+		int n=8;
+		
+		for(int i=1;i<=10;i++){
+			int N = i*n;
+			System.out.println(n + " * " + i + " = " + N);
+		}
+
+	}
+
+}
