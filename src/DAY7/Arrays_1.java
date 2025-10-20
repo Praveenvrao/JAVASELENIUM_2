2bcd65a4c973d9743b0385c83ac89b54b9b185dc
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-21 01:42:32 +0530

    DAY7_Arrays_1

---
+++ src/DAY7/Arrays_1.java
@@ -0,0 +1,14 @@
+package DAY7;
+
+public class Arrays_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n[] = {1,2,3,4};
+		
+		System.out.println(n[0]);
+
+	}
+
+}
