1c3c1f226580719ae4f7238a5da2aa3fc3d0d73e
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-23 16:25:59 +0530

    DAY8_Inheritance_Hirechytype

---
+++ src/DAY8/Inheritance_Hiriercy.java
@@ -0,0 +1,36 @@
+package DAY8;
+
+class PARENT {
+	
+	String method1(String S1, String S2) {
+		return ("This is Parent Method with S1 & S2 which are " + S1 + S2);
+	}
+}
+class Child1 extends PARENT {
+	void childmethod1(String ch1) {
+		System.out.println("Child name is -> " + ch1);
+	}
+}
+class Child2 extends PARENT {
+	void childmethod2(String ch2) {
+		System.out.println("Child 2 name is -> " + ch2);
+	}
+}
+
+public class Inheritance_Hiriercy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Child1 obj1 = new Child1();
+		System.out.println(obj1.method1("MARK C", "MARK B"));
+		obj1.childmethod1("Andrew");
+		Child2 obj2 = new Child2();
+		obj2.childmethod2("Amith");
+		System.out.println(obj2.method1("DAVID BEAN", "NAT"));
+		
+		
+
+	}
+
+}
