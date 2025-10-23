a0cac23b2d61ce08e2af5eab48bb63bd3b712279
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-23 15:40:58 +0530

    DAY8_Inheritance_Singlelevel

---
+++ src/DAY8/Inheritance_1.java
@@ -0,0 +1,29 @@
+package DAY8;
+
+class A {
+	
+	void m1(int a) {
+		System.out.println("This is METHOD1 and the a value is " + a);
+	}
+}
+
+class B extends A {
+	int m2(int b){
+		return (b);
+	}
+}
+
+public class Inheritance_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A obja = new A();
+		//obja.m1(12);
+		
+		B objb = new B();
+		objb.m1(23);
+		System.out.println(objb.m2(55));
+
+	}
+
+}
