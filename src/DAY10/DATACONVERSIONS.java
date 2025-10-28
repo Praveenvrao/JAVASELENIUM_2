c8a435e00d8ca612d6390406064f79d592d94784
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-28 16:54:14 +0530

    DAY10_WrapperclassesDATACONVERSION_1

---
+++ src/DAY10/DATACONVERSIONS.java
@@ -0,0 +1,27 @@
+package DAY10;
+
+public class DATACONVERSIONS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//DATA Conversion is converting String -> int, double, float, char, Boolean and vice versa  int, double, float, char, Boolean -> String
+		
+		/* int - Integer
+		float - Float
+		double - Double
+		char - Character
+		boolean - Boolean */
+		
+		int a = 100;
+		double d = 11.5;
+		char c = 'A';
+		boolean b = true;
+		
+		String as= String.valueOf(a);
+		System.out.println(as);
+		System.out.println(as + (a + d));
+
+	}
+
+}
