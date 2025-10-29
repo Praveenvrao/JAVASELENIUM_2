96f2a1203b4251ae90199d198d75c32d75191805
Praveenvrao <vpraveenrao453@gmail.com>
2025-10-29 15:47:11 +0530

    DAY10_Handleexception_2

---
+++ src/DAY10/Handleexception_2_checked.java
@@ -0,0 +1,16 @@
+package DAY10;
+
+public class Handleexception_2_checked {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am here just checking the checked exceptions");
+		System.out.println("Program started and it will pause few secs");
+		Thread.sleep(5000);
+		System.out.println("Now the program resumed");
+		System.out.println("Program finished");
+
+	}
+
+}
